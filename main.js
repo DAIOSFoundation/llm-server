52dd2ae384b969495ce411117d3aed6b3dbef58a
Tony <first@daiblab.com>
2025-12-12 01:17:29 +0900

    Auto-open DevTools in dev mode for VRAM log debugging

--- main.js
+++ main.js
@@ -132,6 +132,8 @@ function createWindow() {
   const isDev = !app.isPackaged;
   if (isDev) {
     mainWindow.loadURL('http://localhost:5173');
+    // 개발 모드에서 개발자 도구 자동 열기 (VRAM 로그 확인용)
+    mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
   }
